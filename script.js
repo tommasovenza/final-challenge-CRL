6b359eeba2f588c178c6665df11edd940a2d8ae6
Tommaso <tommasovenza@gmail.com>
2021-09-21 16:31:50 +0200

    add functionality to close mobile menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // menu to click
 const burgerMenu = document.querySelector(".fa-bars")
+const crossHidden = document.querySelector(".cross-hidden")
 
 // all stuff to change
 const containerFlex = document.querySelector(".container.flex")
@@ -10,15 +11,30 @@ const mainNavLi = Array.from(document.querySelectorAll(".main-nav li"))
 const logo = document.querySelector(".logo")
 const menuHidden = document.querySelector(".menu-hidden")
 
-// event click
+// event click on burgerMenu
 burgerMenu.addEventListener("click", () => {
   mainNav.classList.add("d-flex")
   containerFlex.classList.add("height-js")
   containerNav.classList.add("width-js")
   logo.classList.add("d-none-js")
   menuHidden.classList.add("d-none-js")
+  crossHidden.classList.add("active-js")
 
   mainNavLi.forEach((listItem) => {
     listItem.classList.add("margin-js")
+  })
+})
+
+// event click on cross
+crossHidden.addEventListener("click", () => {
+  mainNav.classList.remove("d-flex")
+  containerFlex.classList.remove("height-js")
+  containerNav.classList.remove("width-js")
+  logo.classList.remove("d-none-js")
+  menuHidden.classList.remove("d-none-js")
+  crossHidden.classList.remove("active-js")
+
+  mainNavLi.forEach((listItem) => {
+    listItem.classList.remove("margin-js")
   })
 })
